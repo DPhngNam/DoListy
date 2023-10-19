149ccb152b7ae1409c4c574d3565ea68332dcbcf
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-10-19 21:36:54 +0700

    Start new project

---
+++ DoListy/DoListy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DoListy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DoListy/DoListy/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DoListy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
