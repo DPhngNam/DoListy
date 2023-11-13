4ca5e11b282d596b48d89a704b73c2a9766d21f9
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-11-13 23:24:35 +0700

    Delete Create Task

--- DoListy/DoListy/Pages/DayPage.xaml.cs
+++ DoListy/DoListy/Pages/DayPage.xaml.cs
@@ -4,33 +4,21 @@ using System.Collections.ObjectModel;
 
 public partial class DayPage : ContentPage
 {
-    public static ObservableCollection<Task> TaskToday = new ObservableCollection<Task>();
+   
     public DayPage()
     {
         InitializeComponent();
         BindingContext = this; // Add this line if it's not present
 
-        taskListView.ItemsSource = TaskManager.TaskToday;
-
-        taskListView.ItemTemplate = new DataTemplate(() =>
-        {
-            var textCell = new TextCell();
-            textCell.SetBinding(TextCell.TextProperty, "Title");
-
-            return textCell;
-        });
+        
     }
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        taskListView.ItemsSource = TaskManager.TaskToday;
-    }
+    
 
 
     private async void buttonAddTask_Clicked(object sender, EventArgs e)
     {
-        await Navigation.PushModalAsync(new CreateTaskPage());
+        await Navigation.PushModalAsync(new AddAppointmentPage());
     }
 
     private async void Button_Clicked(object sender, EventArgs e)
