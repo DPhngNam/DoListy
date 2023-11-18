0b44f51966237a71fd9beb4e2e5a7de9303c6caa
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-11-18 22:24:10 +0700

    Fixing navigation to AddAppoinmentPage

--- DoListy/DoListy/Pages/DayPage.xaml.cs
+++ DoListy/DoListy/Pages/DayPage.xaml.cs
@@ -1,57 +1,41 @@
 namespace DoListy.Pages;
-
-using System.Collections.ObjectModel;
-using System.Collections.Generic;
+using Appointment = DoListy.ViewModel.Appointment;
 using CommunityToolkit.Maui.Views;
 using Microsoft.Maui.Controls;
-
+using Syncfusion.Maui.Scheduler;
 public partial class DayPage : ContentPage
 {
-    private Dictionary<DateTime, List<IView>> TaskDaily;
+   
     public DayPage()
     {
         InitializeComponent();
         SetIniDisplayDate();
-        TaskDaily = new Dictionary<DateTime, List<IView>>();
+        
     }
 
     private void SetIniDisplayDate()
     {
-        mon.DisplayDate = new DateTime(2023, 11, 13);
-        tue.DisplayDate = new DateTime(2023, 11, 14);
-        wed.DisplayDate = new DateTime(2023, 11, 15);
-        thus.DisplayDate = new DateTime(2023, 11, 16);
-        fri.DisplayDate = new DateTime(2023, 11, 17);
-        sat.DisplayDate = new DateTime(2023, 11, 18);
-        sun.DisplayDate = new DateTime(2023, 11, 19);
+        DateTime today = DateTime.Today;
+        int delta = DayOfWeek.Monday - today.DayOfWeek; // Calculate the offset to Monday
+        if (delta > 0)
+            delta -= 7; // Adjust if today is later in the week than Monday
+
+        mon.DisplayDate = today.AddDays(delta);
+        tue.DisplayDate = today.AddDays(delta + 1);
+        wed.DisplayDate = today.AddDays(delta + 2);
+        thus.DisplayDate = today.AddDays(delta + 3);
+        fri.DisplayDate = today.AddDays(delta + 4);
+        sat.DisplayDate = today.AddDays(delta + 5);
+        sun.DisplayDate = today.AddDays(delta + 6);
     }
 
 
     private async void buttonAddTask_Clicked(object sender, EventArgs e)
     {
-        DateTime current = DateTime.Now; // You need to get the actual date/time for the task
-        TaskDaily.Add(current, new List<IView>());
-
-        // Adding a new goal
-        var newGoal = new Label
-        {
-            Text = "A Goal in " + current.ToString() // Using the current date
-        };
-
-        if (TaskDaily.ContainsKey(current))
-        {
-            TaskDaily[current].Add(newGoal);
-        }
-        else
-        {
-            var newGoalList = new List<IView>();
-            newGoalList.Add(newGoal);
-            TaskDaily.Add(current, newGoalList);
-        }
-
-        TaskDailyStack.Children.Add(newGoal);
+        await Navigation.PushModalAsync(new AddAppointmentPage());
     }
 
+    
     private void LeftimaBut_Clicked(object sender, EventArgs e)
     {
         mon.DisplayDate = mon.DisplayDate.AddDays(-7);
@@ -76,55 +60,60 @@ public partial class DayPage : ContentPage
 
     private void AlwaysOnDisplay(DateTime currentDate)
     {
-        TaskDailyStack.Children.Clear();
-        if (TaskDaily.ContainsKey(currentDate))
-        {
-            var numOfGoals = TaskDaily[currentDate].Count;
-            for (int i = 0; i < numOfGoals; i++)
-            {
-                TaskDailyStack.Children.Add(TaskDaily[currentDate][i]);
-            }
-        }
+        
     }
+
+    
+
+  
 
     private void Butmon_Clicked(object sender, EventArgs e)
     {
+        RefreshCurrentFrame();
         AlwaysOnDisplay(mon.DisplayDate);
-
+        
     }
 
     private void Buttue_Clicked(object sender, EventArgs e)
     {
+        RefreshCurrentFrame();
         AlwaysOnDisplay(tue.DisplayDate);
 
     }
 
     private void Butwed_Clicked(object sender, EventArgs e)
     {
+
+        RefreshCurrentFrame();
+
         AlwaysOnDisplay(wed.DisplayDate);
 
     }
 
     private void Butthus_Clicked(object sender, EventArgs e)
     {
+        RefreshCurrentFrame();
         AlwaysOnDisplay(thus.DisplayDate);
 
     }
 
     private void Butfri_Clicked(object sender, EventArgs e)
     {
+        RefreshCurrentFrame();
         AlwaysOnDisplay(fri.DisplayDate);
 
     }
 
     private void Butsat_Clicked(object sender, EventArgs e)
-    {
+    {   
+        RefreshCurrentFrame();
         AlwaysOnDisplay(sat.DisplayDate);
 
     }
 
     private void Butsun_Clicked(object sender, EventArgs e)
     {
+        RefreshCurrentFrame();
         AlwaysOnDisplay(sun.DisplayDate);
 
     }
@@ -140,6 +129,20 @@ public partial class DayPage : ContentPage
         frame_B.IsVisible = true;
         await frame_B.FadeTo(1, 500, Easing.SinInOut); // Fade in
 
+
+    }
+
+    //Reset position
+    private async void RefreshCurrentFrame()
+    {
+        // Move frame_A back to its original position
+        await frame_A.TranslateTo(0, 0, 250, Easing.Linear);
+        Grid.SetColumn(frame_A, 0);
+        Grid.SetRow(frame_A, 0);
+
+        // Reverse the visibility change for frame_B
+        await frame_B.FadeTo(0, 500, Easing.SinInOut); // Fade out
+        frame_B.IsVisible = false;
 
     }
 }
