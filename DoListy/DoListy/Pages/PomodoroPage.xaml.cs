45e875f75a74d93705e489a7eff80bf8d791110f
Duong Thuan Tri <duongthuantri159@gmail.com>
2023-11-26 17:09:07 +0700

    Update PomodoroPage

--- DoListy/DoListy/Pages/PomodoroPage.xaml.cs
+++ DoListy/DoListy/Pages/PomodoroPage.xaml.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
+
 namespace DoListy.Pages;
 
 public partial class PomodoroPage : ContentPage
 {
+  
 	public PomodoroPage()
 	{
 		InitializeComponent();
+		
 	}
+
 }
