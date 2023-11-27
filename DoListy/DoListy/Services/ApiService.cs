35c46694af2714e635525fa2d66c50c257852db3
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-11-28 00:10:25 +0700

    Apply new design for weather

---
+++ DoListy/DoListy/Services/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using WeatherAppClone.Helpers;
+using Newtonsoft.Json;
+
+namespace DoListy.Services
+{
+    public class ApiService
+    {
+        public static  async Task<Root> getWeather(double latitude, double longtitude)
+        {
+            var httpClient =  new HttpClient();
+            var response=await httpClient.GetStringAsync(string.Format("https://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=Asia%2FBangkok", latitude,longtitude) );
+
+            return JsonConvert.DeserializeObject<Root>(response);  
+        }
+
+       
+    }
+}
--- DoListy/DoListy/Weather/WeatherPage.xaml.cs
+++ DoListy/DoListy/Weather/WeatherPage.xaml.cs
@@ -1,3 +1,5 @@
+using DoListy.Services;
+
 namespace DoListy.Weather;
 
 public partial class WeatherPage : ContentPage
@@ -6,4 +8,12 @@ public partial class WeatherPage : ContentPage
 	{
 		InitializeComponent();
 	}
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		var result = await ApiService.getWeather(10.875, 106.625);
+		Humidity.Text = result.current.relative_humidity_2m.ToString();
+		temperature.Text = result.current.temperature_2m.ToString();
+        wind.Text = result.current.wind_speed_10m.ToString();
+    }
 }
