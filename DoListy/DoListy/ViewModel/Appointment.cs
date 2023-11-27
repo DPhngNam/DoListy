9b46c2862eb798b2a677d6dbae9ebab47ff9349e
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2023-11-27 22:13:00 +0700

    switch case

--- DoListy/DoListy/ViewModel/Appointment.cs
+++ DoListy/DoListy/ViewModel/Appointment.cs
@@ -8,7 +8,7 @@ using SQLite;
 
 namespace DoListy.ViewModel
 {
-    [Table ("Appointment")]
+    [Table("Appointment")]
     public class Appointment
     {
         public static int count = 0;
@@ -18,10 +18,36 @@ namespace DoListy.ViewModel
         public DateTime EventEnd { get; set; }
         [Unique]
         public string Name { get; set; }
-        public Color Colorbg { get; set; }
+
+        public string Colorbg {
+            get
+            {
+                string temp="";
+                switch (Colorbg)
+                {
+                    case "red":
+                       temp= "FF0000";
+                       break;
+                    case "blue":
+                        temp = "0000FF";
+                        break;
+                    default:
+                        temp = "0000FF";
+                        break;
+                  
+                }
+
+                return temp;
+            }
+     
+        }
+             
+
+          
         public string Recurrencerule { get; set; }
         public string Note { get; set;}
         public string State { get; set; }
         public ObservableCollection<Reminder> Reminders { get; set; }
+        
     }
 }
