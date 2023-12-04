75275c2001e5876bd40a690d3b65476cd0dee893
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-12-04 22:55:13 +0700

    little adjust in text

--- DoListy/DoListy/Pages/Feedback.xaml.cs
+++ DoListy/DoListy/Pages/Feedback.xaml.cs
@@ -1,11 +1,16 @@
 using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Controls;
 
-namespace DoListy.Pages;
-
-public partial class Feedback : Popup
+namespace DoListy.Pages
 {
-	public Feedback()
-	{
-		InitializeComponent();
-	}
+    public partial class Feedback : Popup
+    {
+        public Feedback()
+        {
+            InitializeComponent();
+        }
+
+        
+
+    }
 }
