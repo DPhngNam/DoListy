d57a6c21b618211aea71a80da8084ef17a09545a
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-12-07 00:12:49 +0700

    New logic for UI weather but still slow

--- DoListy/DoListy/Weather/WeatherPage.xaml.cs
+++ DoListy/DoListy/Weather/WeatherPage.xaml.cs
@@ -8,15 +8,12 @@ public partial class WeatherPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private String s = "";
 	protected override async void OnAppearing()
 	{
 		base.OnAppearing();
 		var result = await ApiService.getWeather(10.823, 106.6296);
-        drop.Source = "drop.png";
-
-        Humidity.Text = result.current.relative_humidity_2m.ToString();
-		temperature.Text = result.current.temperature_2m.ToString() + "oC";
-		wind.Text = result.current.wind_speed_10m.ToString();
+        
 
         switch (result.current.weather_code)
 		{
@@ -25,12 +22,12 @@ public partial class WeatherPage : ContentPage
 				weatherCode.Text = "clear sky";
 				if(result.current.is_day == 1)
 				{
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/smiling-sun.png";
+                    s = "https://img.icons8.com/fluency/96/smiling-sun.png";
 
 				}
 				else
 				{
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/bright-moon.png";
+                    s = "https://img.icons8.com/fluency/96/bright-moon.png";
                 }
 
                 break;
@@ -39,36 +36,36 @@ public partial class WeatherPage : ContentPage
 				weatherCode.Text = "Mainly clear";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/color/96/sun--v1.png";
+                    s = "https://img.icons8.com/color/96/sun--v1.png";
 
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/color/96/night.png";
+                    s = "https://img.icons8.com/color/96/night.png";
                 }
                 break;
 			case 2:
                 weatherCode.Text = "Partly cloudy";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/partly-cloudy-day.png";
+                    s = "https://img.icons8.com/fluency/96/partly-cloudy-day.png";
 
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/partly-cloudy-night.png";
+                    s = "https://img.icons8.com/fluency/96/partly-cloudy-night.png";
                 }
                 break;
 			case 3:
                 weatherCode.Text = "Overcast";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/color/96/partly-cloudy-day--v1.png";
+                    s = "https://img.icons8.com/color/96/partly-cloudy-day--v1.png";
 
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/partly-cloudy-night.png";
+                    s = "https://img.icons8.com/fluency/96/partly-cloudy-night.png";
                 }
                 break;
 
@@ -76,24 +73,24 @@ public partial class WeatherPage : ContentPage
                 weatherCode.Text = "Fog";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/fog-day.png";
+                    s = "https://img.icons8.com/fluency/96/fog-day.png";
 
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/fog-night.png";
+                    s = "https://img.icons8.com/fluency/96/fog-night.png";
                 }
                 break;
 			case 48:
                 weatherCode.Text = "Depositing rime fog";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/fog-day.png";
+                    s = "https://img.icons8.com/fluency/96/fog-day.png";
 
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/fog-night.png";
+                    s = "https://img.icons8.com/fluency/96/fog-night.png";
                 }
                 break;
                 
@@ -103,18 +100,18 @@ public partial class WeatherPage : ContentPage
 			case 61:
                 weatherCode.Text = "Slight rain";
                 
-                weatherImage.Source = "https://img.icons8.com/fluency/96/light-rain.png";
+                s = "https://img.icons8.com/fluency/96/light-rain.png";
 
                 
                 break;
 
             case 63:
                 weatherCode.Text = "Moderate rain";
-                weatherImage.Source = "https://img.icons8.com/fluency/96/moderate-rain.png";
+                s = "https://img.icons8.com/fluency/96/moderate-rain.png";
                 break;
             case 65:
                 weatherCode.Text = "Heavy rain";
-                weatherImage.Source = "https://img.icons8.com/fluency/96/intense-rain.png";
+                s = "https://img.icons8.com/fluency/96/intense-rain.png";
                 break;
 
 
@@ -124,19 +121,19 @@ public partial class WeatherPage : ContentPage
 			case 80:
                 weatherCode.Text = "Slight rain showers";
 
-                weatherImage.Source = "https://img.icons8.com/fluency/96/light-rain.png";
+                s = "https://img.icons8.com/fluency/96/light-rain.png";
 
 
                 break;
 
             case 81:
                 weatherCode.Text = "Moderate rain showers";
-                weatherImage.Source = "https://img.icons8.com/fluency/96/moderate-rain.png";
+                s = "https://img.icons8.com/fluency/96/moderate-rain.png";
                 break;
 
             case 82:
                 weatherCode.Text = "Heavy rain showers";
-                weatherImage.Source = "https://img.icons8.com/fluency/96/intense-rain.png";
+                s = "https://img.icons8.com/fluency/96/intense-rain.png";
                 break;
 
 
@@ -145,11 +142,11 @@ public partial class WeatherPage : ContentPage
                 weatherCode.Text = "Slight Thunderstorm";
                 if(result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/chance-of-storm.png";
+                    s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/plasticine/100/stormy-night.png";
+                    s = "https://img.icons8.com/plasticine/100/stormy-night.png";
                 }
                 break;
 
@@ -157,25 +154,31 @@ public partial class WeatherPage : ContentPage
                 weatherCode.Text = "Moderate Thunderstorm";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/chance-of-storm.png";
+                    s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/plasticine/100/stormy-night.png";
+                    s = "https://img.icons8.com/plasticine/100/stormy-night.png";
                 }
                 break;
             case 99:
                 weatherCode.Text = "Heavy Thunderstorm";
                 if (result.current.is_day == 1)
                 {
-                    weatherImage.Source = "https://img.icons8.com/fluency/96/chance-of-storm.png";
+                    s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
                 }
                 else
                 {
-                    weatherImage.Source = "https://img.icons8.com/plasticine/100/stormy-night.png";
+                    s = "https://img.icons8.com/plasticine/100/stormy-night.png";
                 }
                 break;
 
         }
-	}
+        weatherImage.Source = s;
+        drop.Source = "drop.png";
+        Humidity.Text = result.current.relative_humidity_2m.ToString();
+        temperature.Text = result.current.temperature_2m.ToString() + "oC";
+        windPic.Source = "wind.png";
+        wind.Text = result.current.wind_speed_10m.ToString();
+    }
 }
