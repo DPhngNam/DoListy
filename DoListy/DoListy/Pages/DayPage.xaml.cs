790f77d783413f8fbc5c92eba00a8cf169737bca
Đoàn Phương Nam <22520908@gm.uit.edu.vn>
2023-12-12 13:50:50 +0700

    Apply the new DayPage

--- DoListy/DoListy/Pages/DayPage.xaml.cs
+++ DoListy/DoListy/Pages/DayPage.xaml.cs
@@ -3,14 +3,15 @@ using System.Collections.ObjectModel;
 using DoListy.Services;
 using Appointment = DoListy.ViewModel.Appointment;
 using DoListy.Weather;
+using System.Xml;
+using Syncfusion.Maui.Scheduler;
+using System.Diagnostics;
 
 namespace DoListy.Pages;
 public partial class DayPage : ContentPage
 {
     private readonly IAudioManager audioManager;
-    //Color for task in frame A
-    private Color transparentColor = Color.FromRgba(255, 255, 255, 0);
-    private Color blackColor = Color.FromRgb(255, 255,255); // it is white
+
 
     //set the setting task's day is Now (for tempo)            
     private DateTime temp = DateTime.Now;
@@ -19,8 +20,8 @@ public partial class DayPage : ContentPage
     {
 
         InitializeComponent();
-        AlwaysOnDisplay(DateTime.Now);
         SetIniDisplayDate();
+        TaskDaily.ItemsSource = null;
         this.audioManager = audioManager;
     }
 
@@ -71,14 +72,23 @@ public partial class DayPage : ContentPage
         sun.DisplayDate = sun.DisplayDate.AddDays(7);
     }
 
-    private async void CheckBox_CheckedChanged(object sender, EventArgs e)
+    private async void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        if (sender is CheckBox checkBox && checkBox.IsChecked)
+        if (sender is CheckBox checkBox && checkBox.BindingContext is int appId)
         {
-            var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("tick.mp3"));
-            player.Play();
+            Appointment temp = App.appointmentRepo.GetAppointmentByID(appId);
+            if (temp != null)
+            {
+                temp.IsDone = !temp.IsDone;
+                checkBox.IsChecked = temp.IsDone;
+                App.appointmentRepo.Update(temp);
+                var player = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync("tick.mp3"));
+                player.Play();
+                return;
+            }
         }
     }
+
 
     private async void buttonAddTask_Clicked(object sender, EventArgs e)
     {
@@ -86,34 +96,26 @@ public partial class DayPage : ContentPage
         var add = (AddAppointmentPage)Shell.Current.CurrentPage;
         add.entryStartTime.Text = temp.ToString("F");
         add.pickerDateTime1.SelectedDate = temp;
-        
+
         add.Disappearing += OnAddAppointmentPageDisappearing;
     }
     private void OnAddAppointmentPageDisappearing(object sender, EventArgs e)
     {
         RefreshCurrentFrame();
-        AlwaysOnDisplay(temp);
+
     }
-    private async Task AnimateFrames(Appointment input)
+    private async Task AnimateFrames()
     {
-        
-            frame_A.WidthRequest = 350;
-            await frame_A.TranslateTo(-220, 0, 250, Easing.Linear);
-            Grid.SetColumn(frame_A, 0);
-            Grid.SetRow(frame_A, 1);
 
+        frame_A.WidthRequest = 350;
+        await frame_A.TranslateTo(-220, 0, 250, Easing.Linear);
+        Grid.SetColumn(frame_A, 0);
+        Grid.SetRow(frame_A, 1);
 
-            // Scale back to original size (if there's relevant code for this)
-            frame_B.FindByName<Label>("TaskTitle").Text = input.Name;
-            frame_B.FindByName<Label>("StartTime").Text = input.EventStart.ToString();
-            frame_B.FindByName<Label>("EndTime").Text = input.EventEnd.ToString();
-            //frame_B.FindByName<Label>("State").Text = input.;
-            frame_B.FindByName<Label>("Notes").Text = input.Note;
+        frame_B.IsVisible = true;
 
-            frame_B.IsVisible = true;
+        await frame_B.FadeTo(1, 500, Easing.SinInOut); // Fade in
 
-            await frame_B.FadeTo(1, 500, Easing.SinInOut); // Fade in
-        
     }
 
     //Reset position
@@ -129,171 +131,78 @@ public partial class DayPage : ContentPage
         Grid.SetRow(frame_A, 1);
 
     }
-    private Appointment xxx;
 
 
-    private void AlwaysOnDisplay(DateTime currentDate)
+    public void loadAppointments()
     {
-        TaskDailyStack.Clear();
-        List<Appointment> appointmentsForDate = new ObservableCollection<Appointment>(App.appointmentRepo.GetAppointments())
-            .Where(app => app.EventStart.Date == currentDate.Date)
-            .ToList();
-
-        foreach (Appointment app in appointmentsForDate)
+        List<Appointment> appointments = App.appointmentRepo.GetAppointments();
+        var AppointmentEvents = new ObservableCollection<Appointment>(appointments);
+        mon.AppointmentsSource = AppointmentEvents;
+        tue.AppointmentsSource = AppointmentEvents;
+        wed.AppointmentsSource = AppointmentEvents;
+        thus.AppointmentsSource = AppointmentEvents;
+        fri.AppointmentsSource = AppointmentEvents;
+        sat.AppointmentsSource = AppointmentEvents;
+        sun.AppointmentsSource = AppointmentEvents;
+    }
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        if (sender is MenuItem menuItem && menuItem.CommandParameter is Appointment appointment)
         {
+            App.appointmentRepo.DeleteAppointment(appointment);
+            loadAppointments();
+            TaskDaily.ItemsSource = null;
+        }
+    }
 
-            // Create labels for displaying appointment information
-            Grid grid = CreateGrid();
+    private void Scheduler_Tapped(object sender, Syncfusion.Maui.Scheduler.SchedulerTappedEventArgs e)
+    {
+        loadAppointments();
+        if (e.Element is SchedulerElement.ViewHeader)
+        {
+            TaskDaily.ItemsSource = null;
+            //if (e.Appointments == null)
+            //    return;
+            //Tasklist.ItemsSource = e.Appointments;
+            //loadAppointments();
 
-            Label nameLabel = CreateLabel(app.Name, blackColor);
-            CheckBox ckbox = CreateCheckBox();
-            Label dateLabel = CreateDateLabel(app.EventStart, app.EventEnd, blackColor);
+            var CurrentAppointment = new ObservableCollection<Appointment>(App.appointmentRepo.GetAppointments());
+            List<Appointment> appointmennts = new List<Appointment>();
 
-            ckbox.CheckedChanged += CheckBox_CheckedChanged;
-
-            grid.Children.Add(nameLabel);
-            grid.Children.Add(dateLabel);
-            grid.Children.Add(ckbox);
-            Grid.SetRow(nameLabel, 0);
-            Grid.SetColumn(nameLabel, 0);
-
-            Grid.SetRow(dateLabel, 1);
-            Grid.SetColumn(dateLabel, 0);
-
-            Grid.SetRowSpan(ckbox, 2);
-            Grid.SetRow(ckbox, 0);
-            Grid.SetColumn(ckbox, 1);  
-            Grid.SetRowSpan(ckbox, 2);
-            
-
-            // Create a StackLayout to hold labels
-            StackLayout infoStack = new StackLayout();
-            infoStack.Children.Add(grid);
-
-            // Create a Frame to contain appointment information
-            Frame appointmentFrame = new Frame
+            foreach (Appointment app in CurrentAppointment)
             {
-                BackgroundColor = transparentColor,
-                Content = infoStack
-            };
-
-            // Add TapGestureRecognizer to the appointmentFrame
-            appointmentFrame.GestureRecognizers.Add(new TapGestureRecognizer
-            {
-                Command = new Command(async () =>
+                if (app.EventStart.Day <= e.Date.Value.Day && e.Date.Value.Day <= app.EventEnd.Day)
                 {
-                    await AnimateFrames(app); // Use the local variable                   
-                })
-            });
-
-            // Create a StackLayout to hold the frame
-            StackLayout frameStack = new StackLayout();
-            frameStack.Children.Add(appointmentFrame);
-
-
-            // Add the combined StackLayout to TaskDailyStack
-            TaskDailyStack.Children.Add(frameStack);
-
-
+                    appointmennts.Add(app);
+                }
+            }
+            TaskDaily.ItemsSource = appointmennts;
+            loadAppointments();
         }
+    }
 
-        // Separate helper methods to improve readability and maintainability
 
-        Grid CreateGrid()
+    private async void TasksList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+
+        if (TaskDaily.SelectedItem != null)
         {
-            Grid newGrid = new Grid();
-            newGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-            newGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-            newGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            newGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            return newGrid;
+            int temp = ((Appointment)e.SelectedItem).Id;
+            await Shell.Current.GoToAsync($"{nameof(EditAppointmentPage)}?AppId={((Appointment)TaskDaily.SelectedItem).Id}");
+            TaskDaily.ItemsSource = null;
         }
-
-        Label CreateLabel(string text, Color textColor)
-        {
-            return new Label { Text = text, TextColor = textColor };
-        }
-
-        CheckBox CreateCheckBox()
-        {
-            return new CheckBox { IsChecked = false };
-        }
-
-        Label CreateDateLabel(DateTime start, DateTime end, Color textColor)
-        {
-            string formattedDate = $"{start:hh/mm,dd/mm/yy}-{end:hh/mm,dd/mm/yy}";
-            return new Label { Text = formattedDate, TextColor = textColor };
-        }
-
-
-
-
-
-
-
     }
-
-    private void Butmon_Clicked(object sender, EventArgs e)
+    private void TasksList_ItemTapped(object sender, ItemTappedEventArgs e)
     {
-        temp = this.mon.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(mon.DisplayDate);
-
+        TaskDaily.SelectedItem = null;
     }
-
-    private void Buttue_Clicked(object sender, EventArgs e)
-    {
-        temp = this.tue.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(tue.DisplayDate);
-    }
-
-    private void Butwed_Clicked(object sender, EventArgs e)
-    {
-        temp = this.wed.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(wed.DisplayDate);
-    }
-
-    private void Butthus_Clicked(object sender, EventArgs e)
-    {
-        temp = this.thus.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(thus.DisplayDate);
-
-    }
-
-    private void Butfri_Clicked(object sender, EventArgs e)
-    {
-        temp = this.fri.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(fri.DisplayDate);
-
-    }
-
-    private void Butsat_Clicked(object sender, EventArgs e)
-    {
-        temp = this.sat.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(sat.DisplayDate);
-
-    }
-
-    private void Butsun_Clicked(object sender, EventArgs e)
-    {
-        temp = this.sun.DisplayDate;
-        RefreshCurrentFrame();
-        AlwaysOnDisplay(sun.DisplayDate);
-
-    }
-
 
     //Weather
     private string s = "";
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-
+        loadAppointments();
         var result = await ApiService.getWeather(10.823, 106.6296);
 
 
@@ -301,7 +210,7 @@ public partial class DayPage : ContentPage
         {
             //0
             default:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/smiling-sun.png";
@@ -315,7 +224,7 @@ public partial class DayPage : ContentPage
                 break;
 
             case 1:
-               
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/color/96/sun--v1.png";
@@ -327,7 +236,7 @@ public partial class DayPage : ContentPage
                 }
                 break;
             case 2:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/partly-cloudy-day.png";
@@ -339,7 +248,7 @@ public partial class DayPage : ContentPage
                 }
                 break;
             case 3:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/color/96/partly-cloudy-day--v1.png";
@@ -352,7 +261,7 @@ public partial class DayPage : ContentPage
                 break;
 
             case 45:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/fog-day.png";
@@ -364,7 +273,7 @@ public partial class DayPage : ContentPage
                 }
                 break;
             case 48:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/fog-day.png";
@@ -380,7 +289,7 @@ public partial class DayPage : ContentPage
 
 
             case 61:
-               
+
 
                 s = "https://img.icons8.com/fluency/96/light-rain.png";
 
@@ -388,11 +297,11 @@ public partial class DayPage : ContentPage
                 break;
 
             case 63:
-                
+
                 s = "https://img.icons8.com/fluency/96/moderate-rain.png";
                 break;
             case 65:
-                
+
                 s = "https://img.icons8.com/fluency/96/intense-rain.png";
                 break;
 
@@ -401,7 +310,7 @@ public partial class DayPage : ContentPage
 
 
             case 80:
-                
+
 
                 s = "https://img.icons8.com/fluency/96/light-rain.png";
 
@@ -409,19 +318,19 @@ public partial class DayPage : ContentPage
                 break;
 
             case 81:
-                
+
                 s = "https://img.icons8.com/fluency/96/moderate-rain.png";
                 break;
 
             case 82:
-                
+
                 s = "https://img.icons8.com/fluency/96/intense-rain.png";
                 break;
 
 
 
             case 95:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
@@ -433,7 +342,7 @@ public partial class DayPage : ContentPage
                 break;
 
             case 96:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
@@ -444,7 +353,7 @@ public partial class DayPage : ContentPage
                 }
                 break;
             case 99:
-                
+
                 if (result.current.is_day == 1)
                 {
                     s = "https://img.icons8.com/fluency/96/chance-of-storm.png";
@@ -457,12 +366,12 @@ public partial class DayPage : ContentPage
 
         }
         weatherImage.Source = s;
-        
+
     }
 
     private async void weatherImage_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync(nameof(WeatherPage));
-        
+
     }
 }
